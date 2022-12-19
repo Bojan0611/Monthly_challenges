0f1f0906a316d8c47a65951559fc2717acf650a9
Bojan0611 <jbojanowski11@gmail.com>
2022-12-19 23:22:27 +0100

    add 404.html

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -42,5 +43,5 @@ def monthly_challenge(request, month):
             "month": month,
         })
     except:
-        return HttpResponseNotFound("<h1>Not supported</h1>")
+        raise Http404()
         
